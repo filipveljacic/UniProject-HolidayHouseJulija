4a775f8ca3fa2cd0ab74404a3041d564d6b591bf
FilipVeljacic <108244669+FilipVeljacic@users.noreply.github.com>
2022-06-28 01:59:38 +0200

    update4

--- js/smartphoneFix.js
+++ js/smartphoneFix.js
@@ -1,5 +1,5 @@
 let isMobile = window.matchMedia("(any-pointer:coarse)").matches;
-var fixFont = document.getElementById("text");
+var fixFont = document.querySelector("#text") = isMobile.matches;
 
 if (isMobile == true) {
     fixFont.style.fontSize = "11.5px";
